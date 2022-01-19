acb2e9a718481a40882f383f1600768465e3e483
Tak Hozumi <thozumi.10@gmail.com>
2022-01-19 12:52:00 -0500

    Updated app

--- app_plaid.py
+++ app_plaid.py
@@ -4,10 +4,14 @@ import json
 app_info = {
     'name': 'Takuro Hozumi',
     'preferred name': 'Tak Hozumi',
+    'email': 'thozumi.10@gmail.com',
     'resume': 'https://www.dropbox.com/s/ukmliso3tlrmcbm/Resume_Dec2021v1.pdf?dl=0',
+    'phone': '614-352-1138',
+    'job_id': 'b2d47344-cf07-476d-a16a-7674bcf60cb0',
+    'github': 'https://github.com/takhozu10',
     'linkedin': 'https://www.linkedin.com/in/tak-hozumi-13581714/',
     'location': 'Lewis Center, Ohio',
-    'favorite candy': 'Kit Kat',
+    'favorite_candy': 'Kit Kat',
     'superpower': 'FinTech Fan / Credit Card Maestro'
 }
 json_data = json.dumps(app_info)
